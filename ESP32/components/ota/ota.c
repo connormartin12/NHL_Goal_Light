1a00428fb947964c7d4033459dae7c4eb062ac54
connormartin12 <connorm7832@gmail.com>
2023-07-13 08:10:36 -0500

    Fixed include issues

--- ESP32/components/ota/ota.c
+++ ESP32/components/ota/ota.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "esp_http_client.h"
 #include "esp_https_ota.h"
-#include "esp_ota_ops_dup.h"
+#include "esp_ota_ops.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
