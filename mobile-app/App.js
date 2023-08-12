1ffa60d4d1efa2954ff4756cd281a27704cc29ba
connormartin12 <connorm7832@gmail.com>
2023-08-12 10:51:49 -0500

    Installed native stack navigator
    Installed and configured native stack navigator from react navigation

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,10 +1,14 @@
 import React from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import UserInputScreen from './app/screens/UserInputScreen';
 import DeviceConnectionScreen from './app/screens/DeviceConnectionScreen';
 
 export default function App() {
   return (
-    <DeviceConnectionScreen />
+    <NavigationContainer>
+      
+    </NavigationContainer>
   );
 }
