3d95d4b3bf58c50a97af2354a05e840727b676e8
connormartin12 <connorm7832@gmail.com>
2023-08-18 17:16:57 -0500

    Update user_info_struct
    Increased base character array sizes to account for any minor size discrepancies. Added comments to the code to document this.

--- ESP32/components/nvs_storage/nvs_storage.h
+++ ESP32/components/nvs_storage/nvs_storage.h
@@ -6,10 +6,10 @@
 #define PARTITION_NAME "MyNvs"
 
 typedef struct user_info_struct {
-    char wifi_ssid[33];
-    char wifi_password[64];
-    char team[40];
-    char delay[4];
+    char wifi_ssid[35]; // 32 characters is the maximum length of a wifi ssid
+    char wifi_password[65]; // 63 characters is the maximum wifi password length
+    char team[50]; // Maximum NHL team length is 39 characters adding 11 characters for safety
+    char delay[5]; // Maximum delay value possible would be 3 digits at most
 } User_Info;
 
 esp_err_t get_stored_info(User_Info *userSettings);
