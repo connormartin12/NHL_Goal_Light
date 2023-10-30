c057b3a91108a8feaca140fdad191f1d552fec47
connormartin12 <connorm7832@gmail.com>
2023-10-29 21:14:16 -0500

    Maxed out animation to 35 frames
    At 30 fps, animation is maxed out at 35 frames to be a little bit over 1 second long at max.

--- mobile-app/app/components/Animations/ConnectingIndicator.js
+++ mobile-app/app/components/Animations/ConnectingIndicator.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import LottieView from 'lottie-react-native';
 
 function ConnectingIndicator({closeIndicator, visible = false}) {
     if (!visible) return null;
 
+    const animationRef = useRef(null);
+
+    useEffect(() => {
+        animationRef.current?.play(0, 35);
+    }, []);
+
     return (
-        <LottieView 
-            autoPlay={true}
+        <LottieView
             loop={false}
             onAnimationFinish={closeIndicator}
+            ref={animationRef}
             source={require('../../assets/animations/loading-circle.json')}
             style={{
                 height: 50,
